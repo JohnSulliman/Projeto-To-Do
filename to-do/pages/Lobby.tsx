b39e1cd4a680f32498dd6073dc29b45c0bfeb1e7
Guilherme de Carvalho Souza <gui_souza_123@hotmail.com>
2021-09-28 22:23:52 -0300

    lobby changes

--- to-do/pages/Lobby.tsx
+++ to-do/pages/Lobby.tsx
@@ -1,28 +1,37 @@
 import { Button, Fab, Grid, Icon } from '@mui/material';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CreateJob from './CreateJob'
 import styles from '../styles/lobby.module.scss';
+import { TextField } from '@mui/material';
 
-function clicou() {
-    alert('asiydgasiukj')
+function chama() {
+    return (
+        <Grid>
+            <CreateJob></CreateJob>
+        </Grid>
+    )  
 }
 
-const Lobby = () =>
+function Lobby() {
 
-<Grid className={styles.lobby}>
-    <Grid className={styles.menu}><h1>aaa</h1></Grid>
-    <Grid className={styles.app}><h1></h1>
-        <Grid className={styles.teste}>
-            <Grid className={styles.a1}>Nome</Grid>
-            <Grid className={styles.a1}>Tempo de Atividade</Grid>
-            <Grid className={styles.a1}>Data de Registro</Grid>
-            <Grid className={styles.a1}>Status da Atividade</Grid>
-            <Grid className={styles.a1}>Data de Previsão</Grid>
-            <Grid className={styles.a1}>Data de Conclusão</Grid>
-            <Grid className={styles.a1}>Substatus da Atividade</Grid>
+    return (
+        <Grid className={styles.lobby}>
+            <Grid className={styles.menu}><h1>aaa</h1></Grid>
+            <Grid className={styles.app}><h1></h1>
+                <Grid className={styles.teste}>
+                    <Grid className={styles.a1}>Nome</Grid>
+                    <Grid className={styles.a1}>Tempo de Atividade</Grid>
+                    <Grid className={styles.a1}>Data de Registro</Grid>
+                    <Grid className={styles.a1}>Status da Atividade</Grid>
+                    <Grid className={styles.a1}>Data de Previsão</Grid>
+                    <Grid className={styles.a1}>Data de Conclusão</Grid>
+                    <Grid className={styles.a1}>Substatus da Atividade</Grid>
+                </Grid>
+                <Fab onClick={chama} className={styles.plus} color="primary" aria-label="add"></Fab>
+            </Grid>
         </Grid>
-        <Fab onClick={clicou} className={styles.plus} color="primary" aria-label="add"></Fab>
-    </Grid>
-</Grid>
+    )
+}
+
 
 export default Lobby
