84c77370684ec5cde21397f689882f10c0535fcf
Sulliman <johnatantc@hotmail.com>
2021-10-05 21:16:18 -0300

    Criação do Jobs e inicialização da função

--- to-do/pages/Lobby.tsx
+++ to-do/pages/Lobby.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { 
-    Button, 
-    Fab, 
-    Grid, 
-    Icon, 
-    Typography } from '@mui/material';
+import {
+    Button,
+    Fab,
+    Grid,
+    Icon,
+    Typography
+} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import React, { useState } from "react";
-import CreateJob from '../components/CreateJob';
+import Jobs from '../components/Jobs';
 import styles from '../styles/lobby.module.scss';
 import { TextField } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
@@ -16,7 +17,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Select , MenuItem } from '@mui/material';
+import { Select, MenuItem } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
 import Teste from './teste';
 import InputLabel from '@mui/material/InputLabel';
@@ -30,36 +31,46 @@ function refreshPage() {
 function reRender() {
     // calling the forceUpdate() method
     this.forceUpdate();
-  };
+};
 
-function aaa() {
-    <Teste></Teste>
-}
+// function aaa() {
+//     const [jobs, setJobs] = React.useState(undefined);
+
+//     return(
+//         <>
+//             <Grid>
+//                 {jobs.map(jobs => {
+//                     <Jobs key={jobs.id} name={jobs.name}></Jobs>
+//                 })}
+//             </Grid>
+//         </>
+//     )
+// }
 
 
 function Lobby() {
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
-      setOpen(true);
+        setOpen(true);
     };
-  
+
     const handleClose = () => {
-      setOpen(false);
+        setOpen(false);
     };
 
     const handleChange = (event) => {
         setAge(event.target.value);
-      };
+    };
 
     const [age, setAge] = React.useState('');
 
-    return(
+    return (
         <>
             <Grid container className={styles.lobby}>
                 <Grid item className={styles.lobby__menu}>
                     <Grid item>
-                        <Image 
+                        <Image
                             priority
                             className={styles.lobby__menu__image}
                             src="/images/perfil00.png"
@@ -93,47 +104,54 @@ function Lobby() {
                         <span className={styles.lobby__head__title}>Data de Conclusão</span>
                         <span className={styles.lobby__head__title}>Substatus</span>
                     </Grid>
+
                     <Fab onClick={handleClickOpen} className={styles.lobby__button}>
                         <AddIcon />
                     </Fab>
-                    <Dialog open={open} onClose={handleClose}>
-                    <DialogTitle>Nova Atividade</DialogTitle>
-                    <DialogContent>
-                    <div className={styles.test}>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Nome da Atividade"
-                        variant="standard"
-                        size="small"
-                    />
-                    <FormControl required sx={{ m: 1, minWidth: 320 }}>
-                    <InputLabel id="demo-simple-select-required-label">Age</InputLabel>
-                    <Select
-                    labelId="demo-simple-select-required-label"
-                    id="demo-simple-select-required"
-                    value={age}
-                    label="Age"
-                    onChange={handleChange}
-                    >
-                    <MenuItem value={10}>Diário</MenuItem>
-                    <MenuItem value={20}>Semanal</MenuItem>
-                    <MenuItem value={30}>Mensal</MenuItem>
-                    </Select>
-                    </FormControl>
-                    </div>
-                    
-                    </DialogContent>
-                    <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
-                    </DialogActions>
-                </Dialog>
+
+                    <Dialog open={open}>
+                        <DialogTitle>Nova Atividade</DialogTitle>
+
+                        <DialogContent>
+                            <div className={styles.test}>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Nome da Atividade"
+                                    variant="standard"
+                                    size="small" 
+                                />
+
+                                <FormControl required sx={{ m: 1, minWidth: 320 }}>
+                                    <InputLabel id="demo-simple-select-required-label">
+                                        Age
+                                    </InputLabel>
+
+                                    <Select
+                                        labelId="demo-simple-select-required-label"
+                                        id="demo-simple-select-required"
+                                        value={age}
+                                        label="Age"
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={10}>Diário</MenuItem>
+                                        <MenuItem value={20}>Semanal</MenuItem>
+                                        <MenuItem value={30}>Mensal</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                        </DialogContent>
+
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button onClick={aaa}>Subscribe</Button>
+                        </DialogActions>
+                    </Dialog>
                 </Grid>
-                <Button onClick={aaa}>Cancel</Button>
+
+                <Button>Cancel</Button>
                 <Button onClick={refreshPage}>refresh</Button>
-                
             </Grid>
         </>
     )
--- to-do/pages/index.tsx
+++ to-do/pages/index.tsx
@@ -16,53 +16,52 @@ function Login() {
   const router = useRouter();
 
   return (
-    <Lobby></Lobby>
-    // <>
-    //   <Head>
-    //     <title>To-Do Blue-EdTech / Login </title>
-    //     <meta name="description" content="Página de Login da To-Do Blue-EdTech" />
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
+    <>
+      <Head>
+        <title>To-Do Blue-EdTech / Login </title>
+        <meta name="description" content="Página de Login da To-Do Blue-EdTech" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    //   <Grid container justifyContent="center" alignItems="center">
-    //     <Grid item>
-    //       <div className={styles.card}>
-    //         <Typography className={styles.card__title}>
-    //           <h1> To-Do Blue-EdTech </h1>
-    //         </Typography>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item>
+          <div className={styles.card}>
+            <Typography className={styles.card__title}>
+              <h1> To-Do Blue-EdTech </h1>
+            </Typography>
             
-    //         <form>
-    //           <TextField 
-    //             className={styles.card__input} 
-    //             label="Usuário" 
-    //             variant="outlined" 
-    //             size="small" />
-    //         </form>
+            <form>
+              <TextField 
+                className={styles.card__input} 
+                label="Usuário" 
+                variant="outlined" 
+                size="small" />
+            </form>
 
-    //         <form>
-    //           <TextField 
-    //             className={styles.card__input} 
-    //             label="Senha" 
-    //             variant="outlined" 
-    //             type="password"
-    //             size="small" />
-    //         </form>
+            <form>
+              <TextField 
+                className={styles.card__input} 
+                label="Senha" 
+                variant="outlined" 
+                type="password"
+                size="small" />
+            </form>
             
-    //         <Button 
-    //           className={styles.card__button} 
-    //           variant="contained" 
-    //           size="small" 
-    //           onClick={() => router.push('/Lobby')}> 
-    //           Entrar 
-    //         </Button>
+            <Button 
+              className={styles.card__button} 
+              variant="contained" 
+              size="small" 
+              onClick={() => router.push('/Lobby')}> 
+              Entrar 
+            </Button>
 
-    //         <Typography>
-    //           <small> Não possui uma conta? <a href="/Signup">Cadastre-se!</a> </small>
-    //         </Typography>
-    //       </div>
-    //     </Grid>
-    //   </Grid>
-    // </>
+            <Typography>
+              <small> Não possui uma conta? <a href="/Signup">Cadastre-se!</a> </small>
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
